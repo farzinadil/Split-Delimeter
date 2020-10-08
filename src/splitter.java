f714f48c0b6b4d78f730ff75621ade419fdd01cb
farzinadil <farzinadil@gmail.com>
2020-10-08 15:50:38 -0700

    added function to split string based on space delimiter

--- src/splitter.java
+++ src/splitter.java
@@ -1,8 +1,33 @@
 /**
  * Created by farzinadil on 10/8/20.
  */
+import java.util.Scanner;
 public class splitter {
     public static void main(String [] args){
-        
+        System.out.println("Enter String: \n");
+        Scanner imput = new Scanner(System.in);
+        String text = imput.nextLine();
+        String removedComma = "";
+        for (int i = 0; i< text.length(); i++){
+            if (text.charAt(i) == ','){
+
+            }
+            else{
+                removedComma += text.charAt(i);
+            }
+        }
+
+        System.out.println("\n Removed Comea : " + removedComma +"\n");
+
+       // System.out.println("\nEnter Delimiter (one character only)");
+
+        String[] output = removedComma.split("\\s+");
+        for (String x: output)
+        {
+            System.out.println(x);
+        }
+
+
+
     }
 }
