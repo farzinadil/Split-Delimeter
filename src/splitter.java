165e7c7da5c5cd4ee02e7a8e459cff0974f5d05a
farzinadil <farzinadil@gmail.com>
2020-10-09 02:31:32 -0700

    added quartiels, mean, and median functions

--- src/splitter.java
+++ src/splitter.java
@@ -22,18 +22,54 @@ public class splitter {
 
         double sum = 0;
 
-        ArrayList<Double> numArray = new ArrayList<Double>();
+        ArrayList<Double> numArray = new ArrayList<>();
+
 
         String[] output = removedComma.split("\\s+");
+        double sampleMin = Double.valueOf(output[0]);
+        double sampleMax = Double.valueOf(output[0]);
         for (String x: output)
         {
+            if (Double.valueOf(x) > sampleMax){
+                sampleMax = Double.valueOf(x);
+            }
+            if (Double.valueOf(x) < sampleMin){
+                sampleMin = Double.valueOf(x);
+            }
             numArray.add(Double.valueOf(x));
             sum+= Double.valueOf(x);
             System.out.println(x);
         }
+        System.out.println("\n sample range : " + sampleMax + " - " + sampleMin + " = " + (sampleMax - sampleMin ));
         System.out.println("\n find sum: " + sum);
+        double mean = sum/numArray.size();
+        System.out.println("\n find mean: " + sum + " / " + numArray.size() + " = " + mean);
+        double stdvar = Math.sqrt((Math.pow(sum-mean, 2) )/ (numArray.size()-1) );
+        System.out.println("\n Standard Deviation = sqrt(" + Math.pow(sum-mean,2)  +  " / " +(numArray.size()-1) + " ) = " +  stdvar);
         System.out.println("\n find range: " + numArray.get(numArray.size()-1) + " - " + numArray.get(0) + " = " + (numArray.get(numArray.size()-1 ) - numArray.get(0)));
-        
+
+        double q1 = 0;
+        double q3 = 0;
+        double iqr = 0;
+        for (int i = 0; i< numArray.size(); i++){
+            if (numArray.size() % 2 == 0 && (i<(numArray.size()/2))  ){
+                q1 += numArray.get(i);
+
+            }
+            else if (numArray.size()%2 == 0 && (i>=numArray.size()/2)){
+                q3 += numArray.get(i);
+            }
+            else if (numArray.size() % 2 == 0 && (i<(numArray.size()/2))  ){
+                q1 += numArray.get(i);
+            }
+            else if(numArray.size()%2 == 0 && (i>numArray.size()/2)){
+                q3 += numArray.get(i);
+            }
+
+        }
+        System.out.println("\n sum 1st half  : " + q1 + " / n " + numArray.size()/2 + " = "  + q1/(numArray.size()/2));
+        System.out.println("\n sum 2nd half  : " + q3 + " / n " + numArray.size()/2 + " = "  + q3/(numArray.size()/2));
+        System.out.println("\n IQR = " + q3/(numArray.size()/2) + q1/(numArray.size()/2));
 
 
 
