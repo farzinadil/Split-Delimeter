cfe0cd521a18e5ed6a3dcf8d0149b38d1b9f4923
farzinadil <farzinadil@gmail.com>
2020-10-08 18:56:25 -0700

    added range

--- src/splitter.java
+++ src/splitter.java
@@ -1,6 +1,7 @@
 /**
  * Created by farzinadil on 10/8/20.
  */
+import java.util.ArrayList;
 import java.util.Scanner;
 public class splitter {
     public static void main(String [] args){
@@ -19,13 +20,20 @@ public class splitter {
 
         System.out.println("\n Removed Comea : " + removedComma +"\n");
 
-       // System.out.println("\nEnter Delimiter (one character only)");
+        double sum = 0;
+
+        ArrayList<Double> numArray = new ArrayList<Double>();
 
         String[] output = removedComma.split("\\s+");
         for (String x: output)
         {
+            numArray.add(Double.valueOf(x));
+            sum+= Double.valueOf(x);
             System.out.println(x);
         }
+        System.out.println("\n find sum: " + sum);
+        System.out.println("\n find range: " + numArray.get(numArray.size()-1) + " - " + numArray.get(0) + " = " + (numArray.get(numArray.size()-1 ) - numArray.get(0)));
+        
 
 
 
